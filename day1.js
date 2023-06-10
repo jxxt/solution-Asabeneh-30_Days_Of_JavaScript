ae995c6eb16c9563ad814e93e81e52465e3f78c0
Jeet Debnath <jeet.debnath2004@gmail.com>
2023-06-10 19:41:47 +0530

    day 1 solutions (10-06-23)

---
+++ day1.js
@@ -0,0 +1,36 @@
+// comments can make code readable
+
+// Welcome to 30DaysOfJavaScript
+
+/* comments can make code readable,
+easy to reuse and informative */
+
+//variable.js
+let str = "jeet"
+let bool = true
+let und = 1 / Infinity
+let nul = null
+
+// datatypes.js
+console.log(typeof (str))
+console.log(typeof (bool))
+console.log(typeof (und))
+console.log(typeof (nul))
+
+let a, b, c, d
+
+let e = 1
+let f = 2
+let g = 3
+let h = 4
+
+my_name = jeet
+surname = debnath
+
+my_namee = jeet, surnamee = debnath
+
+
+myAge = 19
+yourAge = 18
+console.log(myAge)
+console.log(yourAge)
