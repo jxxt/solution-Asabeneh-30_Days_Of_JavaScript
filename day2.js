7597fc9ef0d62b6876153562609ebf2dd0b3e0e5
Jeet Debnath <jeet.debnath2004@gmail.com>
2023-12-23 10:39:28 +0530

    day 2 solutions (23-12-23)

---
+++ day2.js
@@ -0,0 +1,52 @@
+let challenge = "30 Days Of JavaScript"
+
+console.log(challenge)
+
+console.log(challenge.length)
+
+console.log(challenge.toUpperCase())
+
+console.log(challenge.toLowerCase())
+
+let cutChallenge = challenge.substring(0, 2)
+console.log(cutChallenge)
+
+console.log(challenge.substring(2,))
+
+console.log(challenge.includes("Script"))
+
+console.log(challenge.split())
+
+console.log(challenge.split(" "))
+
+let companies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+console.log(companies.split(","))
+
+console.log(challenge.replace("JavaScript", "Python"))
+
+console.log(challenge.charAt(15))
+
+console.log(challenge.charCodeAt("J"))
+
+console.log(challenge.indexOf("a"))
+
+console.log(challenge.lastIndexOf("a"))
+
+console.log("You cannot end a sentence with because because because is a conjunction".indexOf("because"))
+
+console.log("You cannot end a sentence with because because because is a conjunction".lastIndexOf("because"))
+
+console.log("You cannot end a sentence with because because because is a conjunction".search("because"))
+
+console.log(challenge.trim())
+
+console.log(challenge.startsWith("30"))
+
+console.log(challenge.endsWith("JavaScript"))
+
+console.log(challenge.match("a"))
+
+let a = "30 Days of"
+let b = "JavaScript"
+console.log(a.concat(b))
+
